db83d74fea1d59fbe9451d808adc621dc2059be6
Oleh Sliusar <oleh@olehsliusar.com>
2017-01-27 14:29:59 +0200

    finished blog comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  include ArticlesHelper
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+    @comment.save
+    flash.notice = "Comment added!"
+    redirect_to @comment.article
+  end
+
+  private
+  
+    def comment_params
+      params.require(:comment).permit(:author_name, :body)
+    end
+end
